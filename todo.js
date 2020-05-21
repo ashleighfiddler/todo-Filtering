6b528f2eefdb36ef9b4f28f0f015234d4bcb2dc2
ashleighfiddler <ashleigh.fiddler@gmail.com>
2020-05-20 20:45:01 -0500

    first commit

---
+++ todo.js
@@ -0,0 +1,50 @@
+
+   let arrayOfTodos = [
+    {
+    "userId": 14,
+    "id": 1,
+    "title": "delectus aut autem",
+    "completed": false
+  },
+  {
+    "userId": 20,
+    "id": 2,
+    "title": " second delectus aut autem",
+    "completed": false
+  }]
+
+  const fetchTodos = () => {
+    fetch('https://jsonplaceholder.typicode.com/todos')
+    .then( (response) => response.json())
+    .then( (json) => arrayOfTodos = json)
+  }
+
+  const logTodos = () => {
+    console.log(arrayOfTodos)
+  }
+
+  const populateTodos = () => {
+
+    let todoList = document.getElementById('todo-list');
+    let todoTitle = "";
+    let todoComplete = "";
+
+    for (i=0; i < arrayOfTodos.length; i++) {
+        todoTitle = document.createTextNode(arrayOfTodos[i].title);
+
+        let todoItem = document.createElement("LI");  
+
+        if (arrayOfTodos[i].completed) {
+
+            todoItem.classList.add("isTrue");
+        } else {
+            todoItem.classList.add("isFalse")
+        }
+
+    todoItem.appendChild(todoTitle);
+    
+    todoList.appendChild(todoItem);
+    }        
+    console.log(arrayOfTodos[100].title) // => 14
+    console.log(arrayOfTodos[150].title) // => 20
+  }
